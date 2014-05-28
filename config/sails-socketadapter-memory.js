26847918193e3ea2801bfb4314c5f5fbc9833444
fomojola <oluseyi@alum.mit.edu>
2014-05-27 23:26:56 -0400

    Initial release of the SailsJS memory Socket.IO adapter.

---
+++ config/sails-socketadapter-memory.js
@@ -0,0 +1,6 @@
+/**
+ * Default configuration file for the Sails memory socket.io adapter. If using the memory adapter this file should be copied into the Sails project's config directory.
+ * The memory adapter has no required configuration.
+ */
+module.exports = {
+};
